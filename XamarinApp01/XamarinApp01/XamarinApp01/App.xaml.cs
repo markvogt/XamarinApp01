ac5ccc5b877d3c25bb5e470c87a4ef6faa4e2b60
Mark Edward Vogt <markevogt@outlook.com>
2019-07-28 18:15:37 -0500

    CREATED base Xamarin.Forms app

---
+++ XamarinApp01/XamarinApp01/XamarinApp01/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace XamarinApp01
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            AppCenter.Start("android=c71e9a14-8c0a-47e3-91fd-13a114a21baa;" +
+                              "uwp={Your UWP App secret here};" +
+                              "ios={Your iOS App secret here}",
+                              typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
